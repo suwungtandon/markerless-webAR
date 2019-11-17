8948914c7dc938f54fb0ff16a183a6fac0e88631
Suwung <49417240+suwungtandon@users.noreply.github.com>
2019-11-17 23:41:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                lat: -6.9835,
-                lng: 110.4683,
+                // lat: <your-latitude>,
+                // lng: <your-longitude>,
             },
         },
     ];
